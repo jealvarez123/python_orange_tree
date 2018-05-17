e15003e998bdbc00b1e20c1f2461c2d7a9b633cb
jealvarez123 <jealvarez123@gmail.com>
2018-05-17 09:53:12 -0500

    created the orange and the orange tree

---
+++ orange_tree.py
@@ -0,0 +1,18 @@
+import random
+# class Orange():
+#
+#     def __init__(self, diameter, ripeness):
+#         self.diameter = diameter
+#         self.ripeness = ripeness
+#         print('This orange is {} in diameter and  {} out of 5 in ripeness'.format(self.diameter, self.ripeness))
+#
+
+
+
+# class OrangeTree():
+#
+#     def __init__(self, type)
+#     self.orange = []
+#     self.type = type
+#     self.grow_orange()
+#     print()
